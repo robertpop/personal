b0c73fbf948f5c3731808260f610a60faa6074ea
Robert <robert4jc@gmail.com>
2015-07-21 18:22:16 +0300

    FINAL UDPATE
    Making it a valid JS file.

--- fibo.js
+++ fibo.js
@@ -1,18 +1,20 @@
 <script>
 
-	var fn3 = 2, fn6 = 0, fn = 2, sum = 0;
-	// fn3 = F(n-3), fn6 = F(n-6), fn = Fn;
+	var fibo = function() {
+		
+		var fn3 = 2, fn6 = 0, fn = 2, sum = 0;
+		// fn3 = F(n-3), fn6 = F(n-6), fn = Fn;
 
-	while (fn < 4000000) { //see attached paper notes photo for explanation
+		while (fn < 4000000) { //see attached paper notes photo for explanation
+			sum += fn;
+			fn = 4 * fn3 + fn6;
+			fn6 = fn3;
+			fn3 = fn;
+		}
 		
-		sum += fn;
-		fn = 4*fn3 + fn6;
-		fn6 = fn3;
-		fn3 = fn;
-		
-	}
-	
-	console.log(sum);
-	//return sum;
+		//console.log(sum);
+		return sum;
+	};
+	//fibo();
 
 </script>
